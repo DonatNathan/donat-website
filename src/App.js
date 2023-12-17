bf38495f57f73c521491bccbe453ad1877755d57
DonatNathan <nathan.donat-filliod@epitech.eu>
2023-12-17 21:21:37 +0100

    [ADD] - Basic react project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Countdown from "./modules/Countdown";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Countdown end={'September 3, 2027'}/> */}
+    </div>
+  );
+}
+
+export default App;
