d0d0869bc2305e9c5d263398a8378320e5eb5168
DonatNathan <nathan.donat-filliod@epitech.eu>
2023-12-17 21:21:59 +0100

    [ADD] - The countdown of my dreams

---
+++ src/components/CountdownComp.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CountdownBlock = styled.div`
+
+`;
+
+export const RemainingTime = styled.p`
+    text-align: center;
+    font-size: 30px;
+`;
---
+++ src/modules/Countdown.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+import { CountdownBlock, RemainingTime } from "../components/CountdownComp";
+
+const Countdown = ({end}) => {
+    const [seconds, setSeconds] = useState(0);
+
+    const getTimeUntil = (endCountdown) => {
+        const time = Date.parse(endCountdown) - Date.parse(new Date());
+        setSeconds(Math.floor((time/1000)));
+    };
+    
+    useEffect(() => {
+        getTimeUntil(end);
+    });
+    
+    useEffect(() => {
+        setInterval(() => getTimeUntil(end), 1000)
+    }, [end]);
+
+    const addZero = (num) => {
+        return num < 10 ? '0' + num : num;
+    };
+
+    return (
+        <CountdownBlock>
+            <RemainingTime>{addZero(seconds)}</RemainingTime>
+        </CountdownBlock>
+    );
+};
+
+export default Countdown;
