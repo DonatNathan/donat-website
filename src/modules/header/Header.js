6d516d143abb287fbe04876ac6e2e1ccf7c38ba6
Donat <nathan.donat-filliod@epitech.eu>
2026-05-06 22:27:03 +0200

    add: samaritan first view design

--- src/modules/header/Header.js
+++ src/modules/header/Header.js
@@ -1,14 +1,15 @@
 import { Box, Button, Typography } from "@mui/material";
 import { HashLink as Link } from 'react-router-hash-link';
 import React, { useContext } from "react";
-import { ThemeContext } from "../../utils/themes/ThemeContext";
+import { ThemeContext, themes } from "../../utils/themes/ThemeContext";
+import { IoMdSunny, IoMdMoon } from 'react-icons/io';
 
 const Header = () => {
     
-    const {theme} = useContext(ThemeContext);
+    const {theme, toggleTheme} = useContext(ThemeContext);
 
     return (
-        <Box id="home" sx={{display: "flex", flexWrap: "wrap", flexDirection: "row", justifyContent: "space-around", alignItems: "center", textAlign: "center", backgroundColor: theme.BackgroundColor, paddingTop: "20px", paddingBottom: "30px"}}>
+        <Box id="home" sx={{display: "flex", flexWrap: "wrap", flexDirection: "row", justifyContent: "space-around", alignItems: "center", textAlign: "center", backgroundColor: theme.BackgroundColor, paddingVertical: "50px"}}>
             <Box component={Link} to="#home" sx={{display: "flex", flexDirection: "row", alignItems: "center", textDecoration: "none"}}>
                 <Box component="img" sx={{width: 50}} alt="Black Logo Donat" src="/images/logo.png" />
                 <Typography sx={{fontSize: 30, fontWeight: "bold", color: theme.BoldTextColor}}>Donat</Typography>
@@ -19,6 +20,9 @@ const Header = () => {
                 <Link to={"#process"} style={{color: theme.BoldTextColor, textDecoration: "none", margin: "10px"}}>Process</Link>
                 <Link to={"#portfolio"} style={{color: theme.BoldTextColor, textDecoration: "none", margin: "10px"}}>Portfolio</Link>
                 <Link to={"#services"} style={{color: theme.BoldTextColor, textDecoration: "none", margin: "10px"}}>Services</Link>
+                <Button onClick={toggleTheme} style={{backgroundColor: "transparent", color: theme.BoldTextColor, textTransform: "none", fontSize: 15, margin: "10px", minWidth: "auto", padding: "8px"}}>
+                    {theme === themes.dark ? <IoMdSunny size={20} /> : <IoMdMoon size={20} />}
+                </Button>
                 <Button href="#contact" style={{width: "fit-content", backgroundColor: theme.MainColor, color: theme.BackgroundColor, textTransform: "none", fontSize: 15, margin: "15px"}}>Contact</Button>
             </Box>
         </Box>
